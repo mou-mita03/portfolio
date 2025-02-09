58913f79c6b6d74923789ec39b18245c64e8ac13
mou-mita03 <mou81160@gmail.com>
2025-02-09 22:47:14 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing" ,
+{
+  strings:["" , "Learner" ,"Aspiring Web Developer" ,"Problem Solver" , "Tech Enthusiast","Competitive Programmer"],
+  typedSpeed:100,
+  BackSpeed:60,
+  loop:true,
+})
